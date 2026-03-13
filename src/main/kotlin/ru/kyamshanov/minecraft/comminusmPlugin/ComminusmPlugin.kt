c1f31fcb484a4dc86216b0436bb65871f3d9fbc1
KYamshanov <kyamshanov@vk.com>
2026-03-13 16:21:22 +0400

    Initial commit

---
+++ src/main/kotlin/ru/kyamshanov/minecraft/comminusmPlugin/ComminusmPlugin.kt
@@ -0,0 +1,14 @@
+package ru.kyamshanov.minecraft.comminusmPlugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ComminusmPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
