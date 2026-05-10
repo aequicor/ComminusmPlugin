8a8a83d7f022e9c72aab9d767244d4b43e418a9a
KYamshanov <kyamshanov@vk.com>
2026-05-10 11:29:47 +0400

    chore: bump version to 1.0.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ kotlin {
 }
 
 group = "ru.kyamshanov.minecraft"
-version = "1.0.5"
+version = "1.0.6"
 
 // Allow overriding Paper API version via Gradle property `papermcApiVersion`.
 val papermcApiVersion: String = project.findProperty("papermcApiVersion")?.toString() ?: "1.21.11-R0.1-SNAPSHOT"
